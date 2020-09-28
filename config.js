e7ae44e51346f27e14b0c48cf5992e27fa1557fa
hamdy <hamdy.a.farag@gmail.com>
2020-09-28 15:25:46 +0200

    added config file

---
+++ config.js
@@ -0,0 +1,82 @@
+module.exports = {
+
+    nav: {
+        links: [
+        { path: '/github/wiki-2020/', title: 'Docs' }
+        ]
+    },
+    sidebar: [
+        {
+        name: 'wiki2020',
+        sections: [
+            {
+            title: 'Getting Started',
+            items: [
+                '/github/wiki-2020/',
+            ]
+            },
+            {
+            title: 'Decentralized We',
+            items: [
+                '/github/wiki-2020/video-conf/',
+                '/github/wiki-2020/virtual-spaces/',
+                '/github/wiki-2020/video-sharing/',
+                '/github/wiki-2020/website-publisher/',
+                '/github/wiki-2020/wiki-publisher/',
+                '/github/wiki-2020/blog-publisher/',
+            ]
+            },
+            
+            {
+            title: 'Decentralized Me',
+            items: [
+                '/github/wiki-2020/digitalme/',
+                '/github/wiki-2020/crystaltwin/',
+                '/github/wiki-2020/digitalme-id/',	
+            ]
+            },
+            {
+            title: 'Decentralized Office',
+            items: [
+                '/github/wiki-2020/dmcollab/',
+                '/github/wiki-2020/dmcircles/',
+                '/github/wiki-2020/dmoffice/',
+                '/github/wiki-2020/dmcustomers/',
+            ]
+            },
+            {
+            title: 'Decentralized Cloud',
+            items: [
+                '/github/wiki-2020/tfgrid/',		                  
+            ]
+            },
+            {
+            title: 'Decentralized Code',
+            items: [
+                '/github/wiki-2020/gitea/',
+                '/github/wiki-2020/zeroci/',	
+                '/github/wiki-2020/commento/',
+                '/github/wiki-2020/serverless/',	
+                '/github/wiki-2020/gridsome/',
+            ]
+            },
+            {
+            title: 'Decentralized Chain',
+            items: [
+                '/github/wiki-2020/dash/',	
+                '/github/wiki-2020/digibyte/',
+                '/github/wiki-2020/elrond/',
+                '/github/wiki-2020/harmony/',
+                '/github/wiki-2020/matic/',
+                '/github/wiki-2020/neo/',
+                '/github/wiki-2020/skalelabs/',
+                '/github/wiki-2020/stellar/',
+                '/github/wiki-2020/tomochain/',	
+                '/github/wiki-2020/waykichain/',
+                '/github/wiki-2020/upcoming-projects/',
+            ]
+            }
+        ]
+        }
+    ]
+}
