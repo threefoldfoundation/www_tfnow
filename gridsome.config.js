002b65a44237fcead54e13619a2125e1082a8229
hamdy <hamdy.a.farag@gmail.com>
2020-10-15 12:14:38 +0200

    release

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,7 +11,7 @@ module.exports = {
     touchicon: './src/assets/favicon.png'
   },
   siteUrl: (process.env.SITE_URL ? process.env.SITE_URL : ''),
-  pathPrefix: "/wiki10",	
+  pathPrefix: "/wiki2020",	
   settings: {
     web: process.env.URL_WEB || false,
     twitter: process.env.URL_TWITTER || false,
