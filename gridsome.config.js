21013c2b7f755091e0a0f6292b8477b862f025f8
hamdy <hamdy.a.farag@gmail.com>
2020-10-15 15:08:10 +0200

    changes to work in pubtools (staging)

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,7 +11,7 @@ module.exports = {
     touchicon: './src/assets/favicon.png'
   },
   siteUrl: (process.env.SITE_URL ? process.env.SITE_URL : ''),
-  pathPrefix: "/wiki2020",	
+  pathPrefix: "/wiki2020_stag",	
   settings: {
     web: process.env.URL_WEB || false,
     twitter: process.env.URL_TWITTER || false,
