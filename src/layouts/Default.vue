6795e590eb18d8bc5db15d26a85475ac838532a0
hamdy <hamdy.a.farag@gmail.com>
2020-10-29 17:14:07 +0200

    fix crips

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -94,6 +94,17 @@ export default {
   },
   mounted() {
     this.setHeaderHeight();
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "1a5a5241-91cb-4a41-8323-5ba5ec574da0";
+
+    (function() {
+      var d = document;
+      var s = d.createElement("script");
+
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
   },
   metaInfo() {
     return {
