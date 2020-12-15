36081b608399dda7e8a90ba5e1e28157e5e8db06
kristof <kristof@incubaid.com>
2020-12-15 22:30:52 +0100

    init

--- run.sh
+++ run.sh
@@ -10,9 +10,10 @@ popd
 
 rm content/blog content/person content/news content/project
 
-ln -s ~/code/github/threefoldfoundation/data_threefold/content/blog content/blog
-ln -s ~/code/github/threefoldfoundation/data_threefold/content/person content/person
-ln -s ~/code/github/threefoldfoundation/data_threefold/content/news content/news
-ln -s ~/code/github/threefoldfoundation/data_threefold/content/project content/project
+#ln -s ~/code/github/threefoldfoundation/data_threefold/content/blog content/blog
+#ln -s ~/code/github/threefoldfoundation/data_threefold/content/person content/person
+#ln -s ~/code/github/threefoldfoundation/data_threefold/content/news content/news
+#ln -s ~/code/github/threefoldfoundation/data_threefold/content/project content/project
 
 gridsome develop
+
